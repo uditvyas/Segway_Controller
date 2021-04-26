30ceb36f4b77846ec3fd345b9de9ad6e15c22ded
uditvyas <uditvyas23@gmail.com>
2021-04-26 18:21:50 +0530

    Caliberation (K control)

--- segway.m
+++ segway.m
@@ -16,6 +16,9 @@ k3 = m*l;
 k4 = Ir + m*l*l;
 k5 = m*g*l;
 
+initial_theta = pi/12;
+initial_x = -5;
+
 % Open Loop transfer functions
 
 H1 = tf([k2*k3 - k4*k1 0 k1*k5],[k1+k3]);
@@ -47,22 +50,25 @@ sys_ss = ss(A,B,C,D,'statename',states,'inputname',inputs,'outputname',outputs);
 
 sys_tf = tf(sys_ss);
 
-tspan = 0:.01:5;
-y0 = [2; 0; -pi/6; -.5];
-[t,x] = ode45(@(t,x)segway_state_eqns(x,k1, k2, k3, k4, k5, 0),tspan,y0);
-% plot(t, x(:, 1))
+eigs = [-1.1; -3.3;-1.5;-0.7];
+K = place(A, B, eigs);
 
+tspan = 0:0.05:2;
+y0 = [initial_x; 0; initial_theta; 0];
+velocity = 0;
+[t1,x1] = ode45(@(t,x)segway_state_eqns(x,k1, k2, k3, k4, k5, -K*(x - [initial_x + initial_theta*12 + velocity; 0; 0; 0]), pi/20, true),tspan,y0);
+x1(:, 4) = 0;
 
+tspan = 2:0.05:20;
+initial_x = x1(length(x1), 1);
+y0 = x1(length(x1), :);
+y0(4) = 0;
+velocity = x1(length(x1),2);
+[t,x] = ode45(@(t,x)segway_state_eqns(x,k1, k2, k3, k4, k5, -K*(x - [initial_x + initial_theta*12 + velocity; 0; 0; 0]), pi/100, false),tspan,y0);
+final_x = cat(1, x1, x);
+time = cat(1,t1, t);
+% 
 figure
-for q=1:length(t)
-    drawcartpend(x(q,:),m,M,2*l);
+for q=1:length(time)
+    drawcartpend(final_x(q,:),m,M,2*l);
 end
-
-
-eigs = [-1.1; -1.3;-1.5;-1.7];
-K = place(A, B, eigs);  
-
-tspan = 0:0.1:5;
-y0 = [2; 0; pi/12; 0];
-[t,x] = ode45(@(t,x)segway_state_eqns(x,k1, k2, k3, k4, k5, -K*(x - [-2; 0; 0; 0])),tspan,y0);
-
--- segway_state_eqns.m
+++ segway_state_eqns.m
@@ -1,7 +1,13 @@
-function dx = segway_state_eqns(x, k1, k2, k3, k4, k5, u)
-    Sy = sin(x(3));
-    Cy = cos(x(3));
+function dx = segway_state_eqns(x, k1, k2, k3, k4, k5, u, const_theta, holding)
     
+    if holding == true
+        Sy = sin(const_theta);
+        Cy = cos(const_theta);
+        x(4) = 0;
+    else
+        Sy = sin(x(3));
+        Cy = cos(x(3));
+    end
     dx(1,1) = x(2);
     dx(2,1) = (1/(k1*k4 - k2*k3*Cy))*((k4 + k2*Cy)*u + k2*k4*Sy*x(4)^2 - k5*k2*Sy*Cy);
     dx(3,1) = x(4);
